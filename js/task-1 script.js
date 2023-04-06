fa5f176b24f6cb7698abf2bd187f7ff86f7fd84e
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-04-06 19:11:37 +0200

    Completed migration of repo

---
+++ js/task-1 script.js
@@ -0,0 +1,23 @@
+console.log('Task 1 wordt goed ingeladen');
+
+let statCode = 352
+
+prompt('Wat is de landcode?' , '351');
+if (statCode = 352) {
+    alert('Luxemburg');
+    
+} else if (statCode = 32) {
+    alert('België');
+    
+} else if (statCode = 31 ) {
+    alert('Nederland');
+
+} else if (statCode = 62) {
+    alert('Indonesië');
+
+} else if (statCode = 20) {
+    alert('Egypte');
+
+} else {
+    alert('Onbekend');
+}
---
+++ js/task-2 script.js
@@ -0,0 +1,12 @@
+console.log('Task 2 wordt goed ingeladen');
+
+const comNum = ['1,50', '2,50', '5,00', '15,80', '8880,50' ]
+
+for (let i = 0; i < comNum.length; i++) {
+    const comDigit = comNum[i];
+    alert(comDigit);
+}
+
+for (let i = '8880,50'; i > 1,50;) {
+    alert(i);
+}
